d7e5a558b05f5f668d0178818fc3545b31b998f2
tiagoamp <tiagoamp@gmail.com>
2023-10-03 21:49:16 -0300

    Implementa pratica usando excecoes

--- src/aula01/pratica/Main.java
+++ src/aula01/pratica/Main.java
@@ -1,5 +1,6 @@
 package aula01.pratica;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,11 +8,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Informe o primeiro número:");
-        double numero1 = sc.nextDouble();
-
-        System.out.println("Informe o segundo número:");
-        double numero2 = sc.nextDouble();
+        Double numero1 = null, numero2= null;
+        try {
+            System.out.println("Informe o primeiro número:");
+            numero1 = sc.nextDouble();
+            System.out.println("Informe o segundo número:");
+            numero2 = sc.nextDouble();
+        } catch (InputMismatchException e) {  // InputMismatchException é sub-classe de Exception
+            System.out.println("A entrada fornecida nao foi do tipo esperado!!!");
+            return;
+        }
 
         System.out.println("Qual a operação desejada:");
         System.out.println("1 - Adição");
@@ -28,9 +34,22 @@ public class Main {
             case 4 -> operacao = new Divisao();
         }
 
+        try {
+            validarOperacao(operacao);
+        } catch (CalculadoraException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
         System.out.printf("A operação escolhida foi: %s. O resultado é: %.2f.",
                 operacao.getDescricao(),
                 operacao.calcular(numero1, numero2));
+    }
+
+
+    private static void validarOperacao(Operacao operacao) throws CalculadoraException {
+        if (operacao == null)
+            throw new CalculadoraException("Operacao nao identificada!");
     }
 
 }
